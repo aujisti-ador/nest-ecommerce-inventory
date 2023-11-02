0acd93bcb70af7360394729c2e5881165495c2a2
Rabbi, Fazle <fazle.rabbi@alibaba-inc.com>
2023-11-02 19:15:55 +0600

    added time stamp to image

--- src/product-image/entities/product-image.entity.ts
+++ src/product-image/entities/product-image.entity.ts
@@ -1,6 +1,6 @@
 // product-image.entity.ts
 import { Product } from 'src/products/entities/product.entity';
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
 @Entity('product_images')
 export class ProductImage {
@@ -13,4 +13,13 @@ export class ProductImage {
   @ManyToOne(() => Product, (product) => product.images)
   @JoinColumn({ name: 'product_id' })
   product: Product;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @VersionColumn()
+  version: number;
 }
